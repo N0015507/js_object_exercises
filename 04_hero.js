c0c931fd3542c87b6fa7bb3209c683590b0d8afb
Joe Hults <joseph.hults@libertymutual.com>
2017-06-01 15:42:33 -0400

    fixed formatting

--- 04_hero.js
+++ 04_hero.js
@@ -1,36 +1,3 @@
-/**
- * It's time to create a hero to dispatch these pesky monsters.
- *
- * Copy your code from the previous exercise.
- *
- * Add a SETTER method to your LivingThing class named "setHealth" that lets you update the value
- * of the "health" property.
- *
- * Now, create a NEW object called "Hero" that EXTENDS the LivingThing class.
- *
- * NOTE: Check out the following to figure out how to extend an object
- * https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Inheritance
- *
- * Add a method to the Hero class named "attack" that takes as a parameter a LivingThing object.
- * The method should do three things:
- * 1. Reduce the LivingThing object's health by a random value between 0 and 10.
- * 2. Reduce the hero's health by a random value between 0 and 10.
- * 3. Print out how much damage the monster and hero did to each other.
- *
- * NOTE: To point you in the right direction: check out
- * the getRandomInt function on this page:
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
- *
- * Give the Hero object another method named "fight" that takes as a parameter an array of LivingThing objects
- * and does the following:
- *  - For each LivingThing object in the array, call the "attack" method so the hero can attack the monster.
- *     - But, don't attack if the LivingThing is already dead!
- *  - Repeat the process until all the monsters or the hero is dead.
- *
- * Finally, you will need to instantiate your hero object with the name into a variable named "hero". Give them 100 health and a
- * name of your choice.
- */
-
 
 (function() {
   //@see https://stackoverflow.com/questions/1335851/what-does-use-strict-do-in-javascript-and-what-is-the-reasoning-behind-it
@@ -70,101 +37,27 @@
 
   function Hero(monsterName, monsterHealth) {
     LivingThing.call(this, monsterName, monsterHealth);
-
-
     this.attack = function(beingAttacked) {
-
       this.setHealth(this.getHealth() - spentHealth());
       beingAttacked.setHealth(beingAttacked.getHealth() - spentHealth());
       // console.log("beingAttacked " + beingAttacked);
       console.log("Heros health = " + this.getHealth());
       console.log("Monsters health = " + beingAttacked.getHealth());
     }
-
-                    this.fight = function(monsters) {
-                        console.log("IN");
-
-                        for (let i = 0; i <= monsters.length - 1; i++) {
-
-
-                          console.log("i = " + i + " health = " + monsters[i].getHealth() + " MONSTERS NAME = " + monsters[i].getName());
-                          console.log("hero health = " + this.getHealth());
-
-                               while (monsters[i].getHealth() > 0 && this.getHealth() > 0) {
-                                 this.attack(monsters[i]);
-                              //   if (monsters[i].getHealth() > 0) {
-                              //     this.attack(monsters[i]);
-                              console.log("in while");
-                               }
-                      }
-                      //return true;
-                    }
+    this.fight = function(monsters) {
+      console.log("IN");
+      for (let i = 0; i <= monsters.length - 1; i++) {
+        console.log("i = " + i + " health = " + monsters[i].getHealth() + " MONSTERS NAME = " + monsters[i].getName());
+        console.log("hero health = " + this.getHealth());
+        while (monsters[i].getHealth() > 0 && this.getHealth() > 0) {
+          this.attack(monsters[i]);
+          //   if (monsters[i].getHealth() > 0) {
+          //     this.attack(monsters[i]);
+          console.log("in while");
+        }
+      }
+    }
   }
-
-  //////////////////////////
-
-
-  // function LivingThing(monsterName, monsterHealth) {
-  //   var name = monsterName;
-  //   var health = monsterHealth;
-  //
-  //   this.setHealth = function() {
-  //     health = xxxxxx;
-  //   }
-  //
-  //   this.getName = function() {
-  //     return name;
-  //   }
-  //
-  //   this.getHealth = function() {
-  //     return health;
-  //   }
-  //
-  //   this.isAlive = function() {
-  //     if (this.getHealth() > 0) {
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   }
-  // }
-  //
-  // function spentHealth() {
-  //   let rndNum = parseInt(Math.random(1, 10) * 10);
-  //   return rndNum;
-  // }
-  //
-  // function Hero(monsterName, monsterHealth, attack) {
-  //   LivingThing.call(this, monsterName, monsterHealth);
-  //
-  //    console.log("in Hero function ");
-  //    console.log(Hero.monsterName);
-  //               this.attack = function(LivingThing) {
-  //   // console.log("this.attack function ");
-  //                 this.setHealth(this.getHealth() - spentHealth());
-  //                 LivingThing.setHealth(this.getHealth() - spentHealth());
-  //   // console.log("this.health " + this.health);
-  //   // console.log("LivingThing.health = " + LivingThing.health);
-  //                 //return true;
-  //               }
-  //
-  //               this.fight = function(LivingThing) {
-  //                 for (let i = 0; i <= LivingThing.length - 1; i++){
-  //   // console.log("i = " + i);
-  //                   if (LivingThing[i].getHealth > 0){
-  //                     Hero.attack(LivingThing[i]);
-  //                   };
-  //                 }
-  //                 //return true;
-  //               }
-  // }
-  //
-  // let Rat = new LivingThing("Rat", 5);
-  // let Goblin = new LivingThing("Goblin", 30);
-  // let Ogre = new LivingThing("Ogre", 80);
-  // let monsters = [Rat, Goblin, Ogre];
-  // let hero = new Hero("Victor", 100)
-
   //The code below should work when you are done
   console.log("A hero emerges!");
 
